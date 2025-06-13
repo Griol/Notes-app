485073bd72a87c0f73a9a8acaabf30d359015d18
Griol <voytenko02@mail.ru>
2025-06-13 22:06:14 +0300

    depFront

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://notes-app-6yx2.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
